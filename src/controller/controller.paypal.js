900815037a923dbd4b00b19c5fb6f664873beb05
Muhammad Hamza Liaqat <mh408800@gmail.com>
2024-07-30 10:33:52 +0500

    code refactor

--- src/controller/controller.paypal.js
+++ src/controller/controller.paypal.js
@@ -1,58 +1,44 @@
 const braintree = require("braintree");
 
 const gateway = new braintree.BraintreeGateway({
-    environment: braintree.Environment.Sandbox,
-    merchantId: process.env.BRAINTREE_MERCHANT_ID,
-    publicKey: process.env.BRAINTREE_PUBLIC_KEY,
-    privateKey: process.env.BRAINTREE_PRIVATE_KEY,
-})
+  environment: braintree.Environment.Sandbox,
+  merchantId: process.env.BRAINTREE_MERCHANT_ID,
+  publicKey: process.env.BRAINTREE_PUBLIC_KEY,
+  privateKey: process.env.BRAINTREE_PRIVATE_KEY,
+});
 
 const paypalCheckout = async (req, res) => {
-    try {
-      const { amount } = req.body;
-  
-      const result = await gateway.clientToken.generate({});
-  
-      if (!result.success) {
-        return res.status(500).json({ error: 'Failed to generate client token' });
-      }
-  
-      const checkoutRequest = {
-        amount: amount,
-        paymentMethodNonce: "fake-valid-nonce",
-        options: {
-          submitForSettlement: true,
-          paypal: {
-            intent: "sale"
-          },
-          applePay: {
-            total: {
-              label: 'Your Store',
-              amount: amount
-            }
-          },
-          googlePay: {
-            total: {
-              label: 'Your Store',
-              amount: amount
-            }
-          }
-        }
-      };
-  
-      const transactionResult = await gateway.transaction.sale(checkoutRequest);
-  
-      if (transactionResult.success) {
-        res.redirect(transactionResult.transaction.paymentUrl);
-      } else {
-        res.status(500).json({ error: transactionResult.message });
-      }
-    } catch (error) {
-        console.log("an error occured:", error);
-        return res.status(500).json({ message: "an error occured", details: error.message})
+  try {
+    const { amount, paymentMethodNonce } = req.body;
+    if (!amount || !paymentMethodNonce) {
+      return res.status(400).json({ message: "Required fields are missing!" });
     }
-  };
+
+    const result = await gateway.clientToken.generate({});
+    if (!result.success) {
+      return res.status(500).json({ error: 'Failed to generate client token' });
+    }
+
+    const checkoutRequest = {
+      amount: amount,
+      paymentMethodNonce: paymentMethodNonce,
+      options: {
+        submitForSettlement: true,
+      }
+    };
+
+    const transactionResult = await gateway.transaction.sale(checkoutRequest);
+    if (transactionResult.success) {
+      res.status(200).json({ message: 'Transaction successful', transaction: transactionResult.transaction });
+    } else {
+      res.status(500).json({ error: transactionResult.message });
+    }
+  } catch (error) {
+    console.log("An error occurred:", error);
+    return res.status(500).json({ message: "An error occurred", details: error.message });
+  }
+};
 
 module.exports = {
-    paypalCheckout
-}
+  paypalCheckout,
+};
