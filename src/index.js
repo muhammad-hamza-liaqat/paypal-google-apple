b5a4362690c571be3253f85b1c4b9f87cdd5824b
Dev1fd <Dev1@frenchydigital.com>
2024-08-08 10:35:31 +0500

    static sitemap

--- src/index.js
+++ src/index.js
@@ -1,19 +1,17 @@
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
-
-const app = express()
+const { SitemapStream, streamToPromise } = require("sitemap");
+const { createGzip } = require("zlib");
+const app = express();
 require("dotenv").config();
 const { logger, logRequestDuration } = require("./utils/logger");
 
-
-// middleware config
 app.use(express.json());
 app.use(bodyParser.raw({ type: 'application/json' }));
 app.use(cors());
 app.use(logRequestDuration);
 
-// winston middleware Added.
 app.use((req, res, next) => {
   logger.info(`${req.method} ${req.url}`);
   next();
@@ -22,7 +20,35 @@ app.use((req, res, next) => {
 const paypalRoutes = require("./routes/routes.paypal");
 app.use("/api", paypalRoutes);
 
+let sitemap;
+app.get('/sitemap.xml', async (req, res) => {
+  res.header('Content-Type', 'application/xml');
+  res.header('Content-Encoding', 'gzip');
+
+  if (!sitemap) {
+    try {
+      const smStream = new SitemapStream({ hostname: `http://localhost:${process.env.PORT}/` });
+      const pipeline = smStream.pipe(createGzip());
+
+      // smStream.write({ url: '/', changefreq: 'always', priority: 1.0 });
+      smStream.write({ url: 'http://localhost:3000/api/checkout', changefreq: 'daily', priority: 0.8 });
+      smStream.write({ url: 'http://localhost:3000/api/token', changefreq: 'monthly', priority: 0.5 });
+      // smStream.write({ url: '/api/token', changefreq: 'monthly', priority: 0.5 });
+
+      smStream.end();
+
+      const data = await streamToPromise(pipeline);
+      sitemap = data;
+      res.send(data);
+    } catch (e) {
+      console.error(e);
+      res.status(500).end();
+    }
+  } else {
+    res.send(sitemap);
+  }
+});
 
 app.listen(process.env.PORT, () => {
-  console.log(`server is running at http://${process.env.PORT}/`)
-})
+  console.log(`server is running at http://localhost:${process.env.PORT}/`);
+});
